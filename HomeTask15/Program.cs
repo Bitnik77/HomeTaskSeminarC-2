e754113d9e08233357256df399420e2723639003
Bitnik <kukiyshka@gmail.com>
2023-01-25 14:12:10 +0300

    home task complite

---
+++ HomeTask15/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и
+проверяет, является ли этот день выходным */
+Console.Clear();
+Console.Write("Введите число дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 6 || number == 7) Console.WriteLine("Этот день выходной");
+else Console.WriteLine("Этот день не выходной");
